526af13b93e0af82fb8e1200181ac1ef518bf8c0
msjtr <msjt301@gmail.com>
2026-06-09 00:57:53 +0300

    Update dashboard.js

--- assets/  js/dashboard.js
+++ assets/  js/dashboard.js
@@ -1,1 +1,16 @@
+// Dashboard
 
+function loadDashboardStats() {
+
+    const stats = {
+        portfolio: 150000,
+        profits: 12500,
+        available: 3250,
+        investments: 8
+    };
+
+    console.log(stats);
+
+}
+
+loadDashboardStats();
