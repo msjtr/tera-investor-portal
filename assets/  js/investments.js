deea239a960bc8848062c9438aff098301931b2d
msjtr <msjt301@gmail.com>
2026-06-09 00:58:10 +0300

    Update investments.js

--- assets/  js/investments.js
+++ assets/  js/investments.js
@@ -1,1 +1,17 @@
+// Investments
 
+function filterInvestments() {
+
+    const search =
+    document.getElementById('searchInput');
+
+    console.log(search?.value);
+
+}
+
+function viewInvestment(id) {
+
+    window.location.href =
+    `investment-details.html?id=${id}`;
+
+}
