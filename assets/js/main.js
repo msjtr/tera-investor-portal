60e44f088ec220aeb35d4e323f401aea592ef4a4
msjtr <msjt301@gmail.com>
2026-06-09 08:56:17 +0300

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+/* ==========================================
+   TERA Investor Portal
+   Main JS Loader
+========================================== */
+
+// Global
+import './app.js';
+
+// Auth
+import './auth.js';
+import './forgot-password.js';
+
+// Dashboard
+import './dashboard.js';
+
+// Investments
+import './investments.js';
+
+// Portfolio
+import './portfolio.js';
+
+// Profile
+import './profile.js';
+
+// Reports
+import './reports.js';
+
+// Security
+import './security.js';
+
+// Support
+import './support.js';
