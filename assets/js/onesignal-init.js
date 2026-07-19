fcce38e3f2dc9e5f26f460d0eaf52999d7100a2d
msjtr <msjt301@gmail.com>
2026-07-19 03:32:53 +0300

    Update onesignal-init.js

--- assets/js/onesignal-init.js
+++ assets/js/onesignal-init.js
@@ -4,10 +4,17 @@ OneSignalDeferred.push(async function (OneSignal) {
     await OneSignal.init({
         appId: "512d9b65-ec50-41a5-ac12-059a83441a72",
         serviceWorkerPath: "/OneSignalSDKWorker.js",
+        serviceWorkerParam: { scope: "/" },
         notifyButton: {
             enable: false
         }
     });
 
     console.log("✅ OneSignal Initialized");
+
+    const permission = await OneSignal.Notifications.permission;
+    console.log("Notification Permission:", permission);
+
+    const subscribed = await OneSignal.User.PushSubscription.optedIn;
+    console.log("Push Subscribed:", subscribed);
 });
